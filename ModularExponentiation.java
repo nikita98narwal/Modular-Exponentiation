4b6959c266e37af89cdfc476e5b6aaf825ab4231
Nikita Narwal <51452840+nikita98narwal@users.noreply.github.com>
2020-06-02 04:06:22 +0530

    Add files via upload

--- ModularExponentiation.java
+++ ModularExponentiation.java
@@ -1,30 +1,14 @@
-package Mathematics;
+package HackerEarth;
 
 import java.util.Scanner;
+import java.math.*;
 
 public class ModularExponentiation {
-	static int mod(int x, int n, int M) {
-		if(n == 0) {
-			return 1;
-		}
-		else if(n%2 == 0) {
-			int	y = mod(x,n/2,M);
-			return (y * y)%M;
-		}
-		else {
-			return ((x%M) * mod(x,n-1,M))%M;
-		}
-	}
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	    public static void main(String args[]){
 		Scanner sc = new Scanner(System.in);
-		int x = sc.nextInt();
 		int n = sc.nextInt();
-		int M = sc.nextInt();
-		System.out.println(mod(x,n,M));
+		BigInteger m = sc.nextBigInteger();
+		System.out.println(m.mod(BigInteger.valueOf(2).pow(n)));
 		sc.close();
-
 	}
-
 }
